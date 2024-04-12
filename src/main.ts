df80147b0ca849faec22b586789643a93e1fa96c
Данис Иртуганов <danisirtuganov@gmail.com>
2024-04-12 17:20:33 +0300

    Add new origin URL

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'http://92.53.105.37:3000',
+      'http://92.53.105.37',
+    ],
     credentials: true,
     exposedHeaders: 'set-cookie',
   });
